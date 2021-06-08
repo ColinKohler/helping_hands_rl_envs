e6687be53791fd7da5de8591e28f1a6a44bc040a
David Klee <klee.d@northeastern.edu>
2021-06-08 14:56:56 -0400

    adjusted object appearances; cylinder is now similar size as cube, to allow for stacking

--- helping_hands_rl_envs/simulators/pybullet/objects/cylinder.py
+++ helping_hands_rl_envs/simulators/pybullet/objects/cylinder.py
@@ -12,12 +12,12 @@ from helping_hands_rl_envs.simulators import constants
 class Cylinder(PybulletObject):
   def __init__(self, pos, rot, scale):
     root_dir = os.path.dirname(helping_hands_rl_envs.__file__)
-    urdf_filepath = os.path.join(root_dir, constants.URDF_PATH, '8.urdf')
+    urdf_filepath = os.path.join(root_dir, constants.URDF_PATH, 'cylinder.urdf')
     object_id = pb.loadURDF(urdf_filepath, basePosition=pos, baseOrientation=rot, globalScaling=scale)
 
-    super(Cylinder, self).__init__(constants.CUBE, object_id)
+    super(Cylinder, self).__init__(constants.CYLINDER, object_id)
 
-    self.original_height = 0.025
+    self.original_height = 0.05
     self.original_size = 0.05
 
     self.height = self.original_height * scale
