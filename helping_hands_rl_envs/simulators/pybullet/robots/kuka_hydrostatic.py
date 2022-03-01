64a1a1846b252d6ad5baddc0cd66007dd5010d46
w-kyle <cwk.321@gmail.com>
2022-03-01 17:21:17 -0500

    fix pull primitive bug

--- helping_hands_rl_envs/simulators/pybullet/robots/kuka_hydrostatic.py
+++ helping_hands_rl_envs/simulators/pybullet/robots/kuka_hydrostatic.py
@@ -80,11 +80,10 @@ class KukaHydrostaticGripper(RobotBase):
 
   def closeGripper(self, max_it=100, primative=constants.PICK_PRIMATIVE):
     ''''''
-    # if primative == constants.PULL_PRIMATIVE: #TODO BUG w constants
-    #   force = 20
-    # else:
-    #   force = 10
-    force = 10
+    if primative == constants.PULL_PRIMATIVE:
+      force = 20
+    else:
+      force = 10
     p1, p2 = self._getGripperJointPosition()
     target = self.gripper_joint_limit[0]
     self._sendGripperCommand(target, target, force)
