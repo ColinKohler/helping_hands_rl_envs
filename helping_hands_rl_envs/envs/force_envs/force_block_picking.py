5858a9410ff7e6b776988309c6539711f98b0a24
ColinKohler <colink78@gmail.com>
2022-03-04 15:04:22 -0500

    Adding missing paren

--- helping_hands_rl_envs/envs/force_envs/force_block_picking.py
+++ helping_hands_rl_envs/envs/force_envs/force_block_picking.py
@@ -10,7 +10,7 @@ class ForceBlockPickingEnv(CloseLoopBlockPickingEnv):
     state, hand_obs, obs = super()._getObservation(action=action)
     finger_a_force, finger_b_force = self.robot.getFingerForceVector()
 
-    finger_force = [np.sqrt(np.sum(finger_a_force)**2, np.sqrt(np.sum(finger_a_force)**2]
+    finger_force = [np.sqrt(np.sum(finger_a_force)**2), np.sqrt(np.sum(finger_a_force)**2)]
 
     return state, hand_obs, obs, np.array(finger_force)
 
