91e3d0ddb2b067bafdf86089cdf45177883ecdb5
Colin Kohler <colink78@gmail.com>
2022-03-08 17:41:35 -0500

    Added some basic plotting funcitonality. Needs additional work/testing.

--- helping_hands_rl_baselines/logger/plotter.py
+++ helping_hands_rl_baselines/logger/plotter.py
@@ -12,40 +12,61 @@ class Plotter(object):
   '''
   Plotting utility.
   '''
-  def __init__(self):
-    pass
+  def __init__(self, log_filepaths, log_names):
+    self.logs = self.loadLogs(log_filepaths, log_names)
 
-  def plotLearningCurves(self, eps_rewards, title, legend_titles, filepath, window=100):
+  def loadLogs(self, filepaths, names):
+    '''
+
+    '''
+    logs = dict()
+
+    for n, fp in zip(names, filepaths):
+      if os.path.exists(fp):
+        with open(fp, 'rb') as f:
+          logs[n] = append(pickle.load(f))
+      else:
+        print('No log found at {}'.format(f))
+
+    return logs
+
+  def plotLearningCurves(self, title, filepath, window=100):
     '''
     Plot mulitple learning curves on a single plot.
 
     Args:
-      eps_rewards ():
-      title (str):
-      legend_titles (list[str]):
-      filepath (str):
-      window (int):
     '''
-    pass
 
-  def plotLearningCurve(self, eps_rewards, title, filepath, window=100):
+    fig, ax = plt.subplots()
+    ax.set_title(title)
+    ax.set_xlabel('Episodes')
+    ax.set_ylabel('Avg. Reward')
+    ax.legend()
+
+    for log_name, log in self.logs.values():
+      eps_rewards = log['training_eps_rewards']
+      avg_reward = np.mean(list(more_itertools.windowed(eps_rewards, window)), axis=1)
+      xs = np.arange(window, len(avg_reward) + window)
+      ax.plot(xs, avg_reward, label=log_name)
+
+    plt.savefig(filepath)
+    plt.close()
+
+  def plotLearningCurve(self, name, title, filepath, window=100):
     '''
     Plot the learning curve for the given episode rewards.
 
     Args:
-      eps_rewards (list[double]): Episode rewards to plot
-      title (str): Figure title
-      filepath (str): Filepath to save the plot to
-      window (int): Window to average episode rewards over. Default: 100
     '''
-    avg_reward = np.mean(list(more_itertools.windowed(eps_rewards, window)), axis=1)
-    xs = np.arange(window, len(avg_reward) + window)
-
     fig, ax = plt.subplots()
     ax.set_title(title)
-    ax.set_xlabel('Iterations')
+    ax.set_xlabel('Episodes')
     ax.set_ylabel('Avg. Reward')
 
+    eps_reward = self.logs[name]['training_eps_rewards']
+    avg_reward = np.mean(list(more_itertools.windowed(eps_rewards, window)), axis=1)
+    xs = np.arange(window, len(avg_reward) + window)
     ax.plot(xs, avg_reward)
+
     plt.savefig(filepath)
     plt.close()
