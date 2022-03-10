Small bug fix to magnitude calculation in force envs

helping_hands_rl_envs/envs/force_envs/force_block_pulling.py
@@ -10,7 +10,7 @@ class ForceBlockPullingEnv(CloseLoopBlockPullingEnv):
     state, hand_obs, obs = super()._getObservation(action=action)
     finger_a_force, finger_b_force = self.robot.getFingerForce()
 
-    finger_force = [np.sqrt(np.sum(finger_a_force)**2), np.sqrt(np.sum(finger_a_force)**2)]
+    finger_force = [np.sqrt(np.sum(finger_a_force[:3])**2), np.sqrt(np.sum(finger_b_force[:3])**2)]
 
     return state, hand_obs, obs, np.array(finger_force)
 