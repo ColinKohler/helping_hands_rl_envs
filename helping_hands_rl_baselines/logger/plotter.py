07eae68d37c5552b424c42e879c85c07a012f801
Colin Kohler <colink78@gmail.com>
2022-03-14 15:23:21 -0400

    Added plotting tools and block pulling corner env

--- helping_hands_rl_baselines/logger/plotter.py
+++ helping_hands_rl_baselines/logger/plotter.py
@@ -2,6 +2,8 @@
 .. moduleauthor: Colin Kohler <github.com/ColinKohler>
 '''
 
+import os
+import pickle
 import numpy as np
 import more_itertools
 import matplotlib.pyplot as plt
@@ -24,13 +26,13 @@ class Plotter(object):
     for n, fp in zip(names, filepaths):
       if os.path.exists(fp):
         with open(fp, 'rb') as f:
-          logs[n] = append(pickle.load(f))
+          logs[n] = pickle.load(f)
       else:
         print('No log found at {}'.format(f))
 
     return logs
 
-  def plotLearningCurves(self, title, filepath, window=100):
+  def plotLearningCurves(self, title, filepath, window=100, max_eps=None):
     '''
     Plot mulitple learning curves on a single plot.
 
@@ -41,14 +43,16 @@ class Plotter(object):
     ax.set_title(title)
     ax.set_xlabel('Episodes')
     ax.set_ylabel('Avg. Reward')
-    ax.legend()
 
-    for log_name, log in self.logs.values():
+    for log_name, log in self.logs.items():
       eps_rewards = log['training_eps_rewards']
+      if max_eps:
+        eps_rewards = eps_rewards[:max_eps]
       avg_reward = np.mean(list(more_itertools.windowed(eps_rewards, window)), axis=1)
       xs = np.arange(window, len(avg_reward) + window)
       ax.plot(xs, avg_reward, label=log_name)
 
+    ax.legend()
     plt.savefig(filepath)
     plt.close()
 
