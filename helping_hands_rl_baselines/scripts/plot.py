a48e2c151b0600f654002e326996dc6e20459154
Colin Kohler <colink78@gmail.com>
2022-05-25 11:40:22 -0400

    Small updates

--- helping_hands_rl_baselines/scripts/plot.py
+++ helping_hands_rl_baselines/scripts/plot.py
@@ -7,15 +7,17 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   log_filepaths = [
-    '/home/colin/hdd/workspace/midichlorians/data/peg_insertion/md_tol_1/log_data.pkl',
-    '/home/colin/hdd/workspace/ysalamir/data/peg_insertion/md_tol_1/log_data.pkl',
+    '/home/colin/hdd/workspace/midichlorians/data/block_pulling_corner/new_3/log_data.pkl',
+    '/home/colin/hdd/workspace/ysalamir/data/block_pulling_corner/finger_force/log_data.pkl',
+    '/home/colin/hdd/workspace/ysalamir/data/block_pulling_corner/no_finger_force/log_data.pkl',
   ]
-  log_names = ['vison+force', 'vision']
+  #log_names = ['vison+force', 'vision']
+  log_names = ['force_scalar', 'force_image', 'vision']
 
   base_dir = 'helping_hands_rl_baselines/scripts/outputs/'
   plotter = Plotter(log_filepaths, log_names)
   plotter.plotLearningCurves('128x128 Square Peg Insertion', base_dir + 'train.pdf', window=100)
-  plotter.plotEvalRewards('128x128 Square Peg Insertion', base_dir + 'eval_rewards.pdf', window=2)
-  plotter.plotEvalReturns('128x128 Square Peg Insertion', base_dir +'eval_returns.pdf', window=2)
-  plotter.plotEvalLens('128x128 Square Peg Insertion', base_dir + 'eval_lens.pdf', window=2)
-  plotter.plotEvalValues('128x128 Square Peg Insertion', base_dir +'eval_values.pdf', window=2)
+  plotter.plotEvalRewards('128x128 Square Peg Insertion', base_dir + 'eval_rewards.pdf', window=5)
+  plotter.plotEvalReturns('128x128 Square Peg Insertion', base_dir +'eval_returns.pdf', window=5)
+  plotter.plotEvalLens('128x128 Square Peg Insertion', base_dir + 'eval_lens.pdf', window=5)
+  plotter.plotEvalValues('128x128 Square Peg Insertion', base_dir +'eval_values.pdf', window=5)
--- helping_hands_rl_envs/envs/force_envs/force_block_pulling_corner.py
+++ helping_hands_rl_envs/envs/force_envs/force_block_pulling_corner.py
@@ -4,16 +4,22 @@ from helping_hands_rl_envs.envs.close_loop_envs.close_loop_block_pulling_corner 
 class ForceBlockPullingCornerEnv(CloseLoopBlockPullingCornerEnv):
   def __init__(self, config):
     super().__init__(config)
+    self.prev_force = None
 
   def _getObservation(self, action=None):
     ''''''
     state, hand_obs, obs = super()._getObservation(action=action)
-    finger_a_force, finger_b_force = self.robot.getFingerForce()
 
+    #finger_a_force, finger_b_force = self.robot.getFingerForce()
     #finger_force = [np.sqrt(np.sum(finger_a_force[:3])**2), np.sqrt(np.sum(finger_b_force[:3])**2)]
-    finger_force = [finger_a_force[:3], finger_b_force[:3]]
+    #finger_force = np.array([finger_a_force[:3], finger_b_force[:3]]).reshape(-1)
 
-    return state, hand_obs, obs, np.array(finger_force).reshape(-1)
+    force, moment = self.robot.getWristForce()
+    force = np.concatenate((force, moment))
+    #diff_force = force - self.prev_force if self.prev_force is not None else force
+
+    self.prev_force = force
+    return state, hand_obs, obs, force
 
 def createForceBlockPullingCornerEnv(config):
   return ForceBlockPullingCornerEnv(config)
